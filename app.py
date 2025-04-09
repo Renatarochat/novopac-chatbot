8edabe129c3cd0868da4e3a69310436222fcbe2d
Renatarochat <66390545+Renatarochat@users.noreply.github.com>
2025-04-08 21:55:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ data = carregar_dados()
 # Mostrar título e descrição
 col1, col2 = st.columns([1, 8])
 with col1:
-    st.image("logo.png", width=80)  # Substitua por sua logo
+ #   st.image("logo.png", width=80)  # Substitua por sua logo
 with col2:
     st.markdown("### **Assistente virtual do NOVO PAC**")
 
